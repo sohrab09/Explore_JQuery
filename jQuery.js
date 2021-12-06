6ddfc9b71a9f4b1ffd3952e474cb99ff286c4ce8
sohrab09 <sohrab.cse9@gmail.com>
2021-12-06 16:35:56 +0600

    jQuery other selectors added

--- jQuery.js
+++ jQuery.js
@@ -1,8 +1,12 @@
+// Document select by ID 
+
 $(document).ready(function(){
     $("#head").mouseover(function(){
         $("h1").css("color","red",);
     });
 });
+
+// Document select by Class 
 
 $(document).ready(function(){
     $(".button1").click(function() {
@@ -12,5 +16,15 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".button2").click(function() {
         $("p").show();
+    });
+});
+
+
+
+// if we want to select all the elements in the HTML document, we can use the following syntax:
+
+$(document).ready(function(){
+    $("button").click(function() {
+        $("*").hide();
     });
 });
